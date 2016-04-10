20f70dc0fba6f7170f10d9d777f2c365dd47eb34
Manne Busk <mannebusk@gmail.com>
2016-04-10 13:04:42 +0200

    Added empty overrides on core email models

---
+++ app/code/community/Pulchritudinous/Email/Model/Core/Email.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Core Email Override
+ *
+ * @package Pulchritudinous_Email
+ * @module  Pulchritudinous
+ * @author  Puchtritudinous
+ */
+Class Pulchritudinous_Email_Model_Core_Email
+    extends Mage_Core_Model_Email
+{
+}
---
+++ app/code/community/Pulchritudinous/Email/Model/Core/Email/Template.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Core Email Template Override
+ *
+ * @package Pulchritudinous_Email
+ * @module  Pulchritudinous
+ * @author  Puchtritudinous
+ */
+Class Pulchritudinous_Email_Model_Core_Email_Template
+    extends Mage_Core_Model_Email_Template
+{
+}
