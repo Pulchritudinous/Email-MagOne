6fe8e219e4e702e056515a4819c1b35bff9c58aa
Manne Busk <mannebusk@gmail.com>
2016-04-10 15:24:39 +0200

    Added basic send functions for core/email and core/email_template

--- app/code/community/Pulchritudinous/Email/Model/Core/Email.php
+++ app/code/community/Pulchritudinous/Email/Model/Core/Email.php
@@ -9,4 +9,19 @@
 Class Pulchritudinous_Email_Model_Core_Email
     extends Mage_Core_Model_Email
 {
+    /**
+     * Send Email
+     *
+     * @return Pulchritudinous_Email_Model_Core_Email
+     */
+    public function send()
+    {
+        $config = Mage::helper('pulchemail/config');
+
+        if (!$config->isEnabled()) {
+            return parent::send();
+        }
+
+        $email = Mage::getModel('pulchemail/email');
+    }
 }
--- app/code/community/Pulchritudinous/Email/Model/Core/Email/Template.php
+++ app/code/community/Pulchritudinous/Email/Model/Core/Email/Template.php
@@ -9,4 +9,23 @@
 Class Pulchritudinous_Email_Model_Core_Email_Template
     extends Mage_Core_Model_Email_Template
 {
+    /**
+     * Send mail to recipient
+     *
+     * @param   array|string       $email        E-mail(s)
+     * @param   array|string|null  $name         receiver name(s)
+     * @param   array              $variables    template variables
+     * @return  boolean
+     **/
+    public function send($email, $name = null, array $variables = array())
+    {
+        $config = Mage::helper('pulchemail/config');
+
+        if (!$config->isEnabled()) {
+            parent::send($email, $name, $variables);
+            return;
+        }
+
+        $email = Mage::getModel('pulchemail/email');
+    }
 }
