75d15a1693bf89adf8a8b05cc52893ac5df85742
Anton Samuelsson <samuelsson.anton@gmail.com>
2016-04-18 20:35:14 +0200

    Fixed core override method, needed to override even more

--- app/code/community/Pulchritudinous/Email/Model/Core/Email/Template.php
+++ app/code/community/Pulchritudinous/Email/Model/Core/Email/Template.php
@@ -10,23 +10,104 @@ Class Pulchritudinous_Email_Model_Core_Email_Template
     extends Mage_Core_Model_Email_Template
 {
     /**
-     * Zend email class.
+     * Send mail to recipient
      *
-     * Sets the default transporter if active.
-     *
-     * @return Zend_Mail
-     */
-    public function getMail()
+     * @param   array|string       $email        E-mail(s)
+     * @param   array|string|null  $name         receiver name(s)
+     * @param   array              $variables    template variables
+     * @return  boolean
+     **/
+    public function send($email, $name = null, array $variables = array())
     {
         $config = Mage::helper('pulchemail/config');
         $helper = Mage::helper('pulchemail');
-        $mail   = parent::getMail();
 
-        if ($config->isEnabled()) {
-            $mail::setDefaultTransport($helper->getActiveTransporter());
+        if (!$config->isEnabled()) {
+            return parent::send($email, $name, $variables);
         }
 
-        return $mail;
+        if (!$this->isValidForSend()) {
+            Mage::logException(new Exception('This letter cannot be sent.')); // translation is intentionally omitted
+            return false;
+        }
+
+        $emails = array_values((array)$email);
+        $names = is_array($name) ? $name : (array)$name;
+        $names = array_values($names);
+        foreach ($emails as $key => $email) {
+            if (!isset($names[$key])) {
+                $names[$key] = substr($email, 0, strpos($email, '@'));
+            }
+        }
+
+        $variables['email'] = reset($emails);
+        $variables['name'] = reset($names);
+
+        $this->setUseAbsoluteLinks(true);
+        $text = $this->getProcessedTemplate($variables, true);
+        $subject = $this->getProcessedTemplateSubject($variables);
+
+        $setReturnPath = Mage::getStoreConfig(self::XML_PATH_SENDING_SET_RETURN_PATH);
+        switch ($setReturnPath) {
+            case 1:
+                $returnPathEmail = $this->getSenderEmail();
+                break;
+            case 2:
+                $returnPathEmail = Mage::getStoreConfig(self::XML_PATH_SENDING_RETURN_PATH_EMAIL);
+                break;
+            default:
+                $returnPathEmail = null;
+                break;
+        }
+
+        if ($this->hasQueue() && $this->getQueue() instanceof Mage_Core_Model_Email_Queue) {
+            /** @var $emailQueue Mage_Core_Model_Email_Queue */
+            $emailQueue = $this->getQueue();
+            $emailQueue->setMessageBody($text);
+            $emailQueue->setMessageParameters(array(
+                    'subject'           => $subject,
+                    'return_path_email' => $returnPathEmail,
+                    'is_plain'          => $this->isPlain(),
+                    'from_email'        => $this->getSenderEmail(),
+                    'from_name'         => $this->getSenderName(),
+                    'reply_to'          => $this->getMail()->getReplyTo(),
+                    'return_to'         => $this->getMail()->getReturnPath(),
+                ))
+                ->addRecipients($emails, $names, Mage_Core_Model_Email_Queue::EMAIL_TYPE_TO)
+                ->addRecipients($this->_bccEmails, array(), Mage_Core_Model_Email_Queue::EMAIL_TYPE_BCC);
+            $emailQueue->addMessageToQueue();
+
+            return true;
+        }
+
+        $mail = $this->getMail();
+
+        $mail::setDefaultTransport($helper->getActiveTransporter());
+
+        foreach ($emails as $key => $email) {
+            $mail->addTo($email, '=?utf-8?B?' . base64_encode($names[$key]) . '?=');
+        }
+
+        if ($this->isPlain()) {
+            $mail->setBodyText($text);
+        } else {
+            $mail->setBodyHTML($text);
+        }
+
+        $mail->setSubject('=?utf-8?B?' . base64_encode($subject) . '?=');
+        $mail->setFrom($this->getSenderEmail(), $this->getSenderName());
+
+        try {
+            $mail->send();
+            $this->_mail = null;
+        }
+        catch (Exception $e) {
+            $this->_mail = null;
+            Mage::logException($e);
+            return false;
+        }
+
+        return true;
     }
 }
 
