8cc2f49c5b9bc47c95b8a60de22c93f4c6f8ee74
Anton Samuelsson <samuelsson.anton@gmail.com>
2016-04-21 20:50:40 +0200

    Fixed license header

--- app/code/community/Pulchritudinous/Email/Helper/Data.php
+++ app/code/community/Pulchritudinous/Email/Helper/Data.php
@@ -2,7 +2,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2016 Anton Samuelsson
+ * Copyright (c) 2016 Pulchritudinous
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,7 +25,7 @@
 ?>
 <?php
 /**
- *
+ * Generic helper file.
  *
  * @package Pulchritudinous_Email
  * @module  Pulchritudinous
